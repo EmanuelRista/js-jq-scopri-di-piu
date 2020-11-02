dbc9378687e503e1b55899b4fee553e477e40ae5
Emanuel <e.rista@outlook.it>
2020-11-02 16:22:56 +0100

    End

---
+++ script.js
@@ -0,0 +1,5 @@
+// Creare il titolo di un paragrafo e un bottone che permette di espandere il testo inizialmente nascosto;
+$("#my_text").hide();
+$("#my_button").click(function() {
+  $("#my_text").show();
+});
